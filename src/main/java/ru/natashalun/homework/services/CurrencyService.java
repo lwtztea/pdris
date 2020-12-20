7cabbd849bd1d7b62086106bcffc6e287acf844a
Natalia Luneva <61375514+lwtztea@users.noreply.github.com>
2020-12-20 17:50:03 +0200

    edited classes

--- src/main/java/ru/natashalun/homework/services/CurrencyService.java
+++ src/main/java/ru/natashalun/homework/services/CurrencyService.java
@@ -1,10 +1,21 @@
 package ru.natashalun.homework.services;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import ru.natashalun.homework.dao.CurrencyDAO;
 import ru.natashalun.homework.entities.Currency;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -14,10 +25,77 @@ public class CurrencyService {
     private CurrencyDAO currencyDAO;
 
     public Currency getTodayStockExchange() {
-        return currencyDAO.getLastExchangeRates(1).get(0);
+        return getLastStocks(1).get(0);
     }
 
     public List<Currency> getLastStocks(int n) {
-        return currencyDAO.getLastExchangeRates(n);
+        ArrayList<Currency> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            LocalDate date = LocalDate.now().minusDays(i);
+
+            Currency stored = currencyDAO.getExchangeRateForDate(LocalDate.now());
+            if (stored != null) {
+                result.add(stored);
+                continue;
+            }
+
+            String uri = String.format(
+                    "http://www.cbr.ru/scripts/XML_daily.asp?date_req=%s",
+                    date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+            );
+
+            //Я то это переделала на RestTemplate, только теперь появилась куча ненужных классов ¯\_(ツ)_/¯
+            RestTemplate template = new RestTemplate();
+
+            List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+            MappingJackson2XmlHttpMessageConverter converter = new MappingJackson2XmlHttpMessageConverter();
+            converter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_XML));
+            messageConverters.add(converter);
+            template.setMessageConverters(messageConverters);
+
+            ResponseEntity<CurrencyListXML> responseEntity = template.getForEntity(
+                    uri,
+                    CurrencyListXML.class
+            );
+
+            Currency localResult = null;
+            if (responseEntity.getBody() != null) {
+                for (CurrencyListXML.CurrencyXML item : responseEntity.getBody().currencyList){
+                    if (item.name.equals("Доллар США")) {
+                        localResult = item.toCurrency(date);
+                    }
+                }
+            }
+            currencyDAO.setExchangeRateForDate(date, localResult);
+            result.add(localResult);
+        }
+        return result;
+    }
+
+    private static class CurrencyListXML {
+        @JacksonXmlProperty(localName = "Valute")
+        @JacksonXmlElementWrapper(useWrapping = false)
+        private List<CurrencyXML> currencyList;
+
+        static class CurrencyXML {
+            @JacksonXmlProperty(localName = "Name")
+            private String name;
+            @JacksonXmlProperty(localName = "Nominal")
+            private int nominal;
+            @JacksonXmlProperty(localName = "Value")
+            private float value;
+
+            public void setValue(String value) {
+                this.value = Float.parseFloat(value.replace(',', '.'));
+            }
+
+            public Currency toCurrency(LocalDate date) {
+                return new Currency(
+                        name,
+                        value / nominal,
+                        date
+                );
+            }
+        }
     }
 }
--- src/main/java/ru/natashalun/homework/services/WeatherService.java
+++ src/main/java/ru/natashalun/homework/services/WeatherService.java
@@ -1,10 +1,16 @@
 package ru.natashalun.homework.services;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import ru.natashalun.homework.dao.WeatherDAO;
 import ru.natashalun.homework.entities.Weather;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,7 +26,7 @@ public class WeatherService {
     }
 
     public Weather getTodayWeather(String city) {
-        return weatherDAO.getLastWeather(1, city).get(0);
+        return getLastWeather(1, city).get(0);
     }
 
     public List<Weather> getLastWeather(int n) {
@@ -28,6 +34,88 @@ public class WeatherService {
     }
 
     public List<Weather> getLastWeather(int n, String city) {
-        return weatherDAO.getLastWeather(n, city);
+        LocalDate currentDate = LocalDate.now();
+        List<Weather> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            LocalDate date = currentDate.minusDays(i);
+            Weather stored = weatherDAO.getWeather(date, city);
+            if (stored != null) {
+                result.add(stored);
+                continue;
+            }
+
+            String address = String.format(
+                    "http://api.weatherapi.com/v1/history.json?key=%s&q=%s&dt=%s",
+                    "b08521b2c5f145078eb232719201212",
+                    city,
+                    date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+            );
+
+            //Я то это переделала на RestTemplate, только теперь появилась куча ненужных классов ¯\_(ツ)_/¯
+            ResponseEntity<WeatherResponse> responseEntity = new RestTemplate().getForEntity(
+                    address,
+                    WeatherResponse.class
+            );
+
+            if (responseEntity.getBody() != null) {
+                Weather localResult = responseEntity.getBody()
+                        .forecast
+                        .forecastDays
+                        .get(0)
+                        .weatherJSON
+                        .toWeather(date);
+                result.add(localResult);
+                weatherDAO.setWeather(date, city, localResult);
+            }
+        }
+        return result;
+    }
+
+    private static class WeatherResponse {
+        @JsonProperty("forecast")
+        private Forecast forecast;
+
+        static class Forecast {
+            @JsonProperty("forecastday")
+            private List<ForecastDay> forecastDays;
+
+            static class ForecastDay {
+                @JsonProperty("day")
+                private WeatherJSON weatherJSON;
+            }
+
+            public static class WeatherJSON {
+                @JsonProperty("maxtemp_c")
+                private float maxtempC;
+                @JsonProperty("mintemp_c")
+                private float mintempC;
+                @JsonProperty("avgtemp_c")
+                private float avgtempC;
+                @JsonProperty("maxwind_kph")
+                private float maxwindKph;
+                @JsonProperty("totalprecip_mm")
+                private float totalprecipMm;
+                @JsonProperty("avgvis_km")
+                private float avgvisKm;
+                @JsonProperty("avghumidity")
+                private int avghumidity;
+                @JsonProperty("uv")
+                private float uv;
+
+                public Weather toWeather(LocalDate date) {
+                    return new Weather(
+                            maxtempC,
+                            mintempC,
+                            avgtempC,
+                            maxwindKph,
+                            totalprecipMm,
+                            avgvisKm,
+                            avghumidity,
+                            uv,
+                            date
+                    );
+                }
+            }
+        }
     }
 }
